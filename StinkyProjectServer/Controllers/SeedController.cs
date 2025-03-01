c8f83a3ec3e9a26f8c2fe161a24f71ca19cc7a48
Vi <quachdaivy7@gmail.com>
2025-02-28 18:04:16 -0800

    asasasdas

---
+++ StinkyProjectServer/Controllers/SeedController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StinkyModel;
+
+namespace StinkyProjectServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController(WorldcitiessourceContext context) : ControllerBase
+    {
+        private readonly WorldcitiessourceContext _context = context;
+        [HttpPost("Country")]
+        public async Task<ActionResult> ImportCountryAsync (Country country)
+        {
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCity", new { id = city.Id }, city);
+        }
+    }
+}
---
+++ StinkyProjectServer/Dto/CityCountry.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StinkyModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace StinkyProjectServer.Dto
+{
+    public class CityCountry
+    {
+        
+        public int Id { get; set; }
+
+       
+        public string Name { get; set; } = null!;
+
+    
+        public int Population { get; set; }
+
+        public string CountryName { get; set; } = null!;
+
+
+    }
+}
